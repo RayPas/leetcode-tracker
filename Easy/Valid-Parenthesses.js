4b727b5309e5c1fd285de4e10f06817d78190118
RayPas <70732899+RayPas@users.noreply.github.com>
2023-02-01 15:02:00 -0800

    Valid-parenthesses #20

---
+++ Easy/Valid-Parenthesses.js
@@ -0,0 +1,32 @@
+/**
+ * 20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ */
+
+var isValid = function(s) {
+    const lib = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
+
+    //Edge cases
+    if (s.length % 2 === 1) return false;
+
+    let stack = [];
+
+    for(let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i])
+        } else if (lib[stack.pop()] !== s[i]){
+            return false
+        }
+    }
+    return stack.length === 0;
+};
