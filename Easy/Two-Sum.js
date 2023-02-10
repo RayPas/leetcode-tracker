750e7b361249b2b5c56a2a95ab61badff62ff838
RayPas <70732899+RayPas@users.noreply.github.com>
2023-02-10 10:34:09 -0800

    Added a practed Hashmap ver for two-sum

--- Easy/Two-Sum.js
+++ Easy/Two-Sum.js
@@ -1,4 +1,4 @@
-/* 
+/*
 1. Two Sum
 
 
@@ -35,3 +35,21 @@ const twoSum = function (nums, target) {
   //     }
   // }
 };
+
+/**
+ * Hash map solve
+ *
+ * 
+ // Hash map practice variation
+const twoSum = function(nums, target) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        let diff = target - nums[i];
+
+        if(map.has(diff)) return [map.get(diff), i];
+
+        map.set(nums[i], i)
+    }
+}
+ */
